a4dffc301ebb5105158a67a84cbe249143c66ada
Farhan <farhanahmadptn@gmail.com>
2026-05-15 18:49:42 +0000

    Configure Terraform IAC-Database resource

---
+++ modules/database/main.tf
@@ -0,0 +1,30 @@
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "iac-db-subnet-group"
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name        = "iac-db-subnet-group"
+    Environment = "Dev"
+    Project     = "IaC-Core-Cloud"
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  db_name                = "iacdb"
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  deletion_protection    = false
+
+  tags = {
+    Name        = "iac-mysql-db"
+    Environment = "Dev"
+    Project     = "IaC-Core-Cloud"
+  }
+}
